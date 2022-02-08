2d81c7c5dbdced055f6f0c76054ba58f2e182f72
Henry Miller <mail.henrym+git@gmail.com>
2022-02-07 22:41:10 -0500

    commit tree 75c3d10fda4a3de169388a44534c471858ea9166

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Segment } from "semantic-ui-react";
 import "../stylesheets/App.css";
+import WestworldMap from "./WestworldMap";
+import Headquarters from "./Headquarters";``
 
 function App() {
   return (
     <Segment id="app">
+      <WestworldMap/>
+      <Headquarters/>
       {/* What components should go here? Check out Checkpoint 1 of the Readme if you're confused */}
     </Segment>
   );
